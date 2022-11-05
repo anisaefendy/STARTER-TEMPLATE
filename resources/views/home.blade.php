2b61826184bfbd423e8f05a0102989226f58ccd1
amanyasma19 <amanyasma19@gmail.com>
2022-11-05 09:55:02 +0700

    otentikasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,21 @@
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
+    <div class="container">
+        <div class="row justify-content-center">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                <div class="card-header">{{__('Dashboard')}}</div>
+                    <div class="card-body">
+                        @if($user->roles_id == 1)
+                        anda login sebagai admin
+                        @else
+                        anda login sebagai user
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 @stop
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home')->middleware('is_admin');
 
 Auth::routes();
 
